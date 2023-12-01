70493b909482a96b8756b879ffb02ffb97d1f63c
Peter Tseng <pht24@cornell.edu>
2023-12-01 06:59:02 +0000

    lib: readInputFileAndFlags
    Note that 2023 was the first time this was added (it may have been
    backported to other years since then, but 2023 was the first time, due
    to day 1 part 2 input not matching part 1 format, and therefore needing
    a flag to run part 2 only)

--- lib/AdventOfCode.hs
+++ lib/AdventOfCode.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE TupleSections #-}
+
 module AdventOfCode (
   readInputFile
+, readInputFileAndFlags
 ) where
 
+import Data.Char (isAlphaNum)
+import Data.List (partition)
 import System.Environment (getArgs)
 
 readInputFile :: IO String
@@ -11,3 +16,16 @@ readInputFile = do
         [] -> "/dev/stdin"
         a:_ -> a
   readFile f
+
+isFlag :: String -> Bool
+isFlag ('-':x:_) = isAlphaNum x
+isFlag _ = False
+
+readInputFileAndFlags :: IO (String, [(Char, String)])
+readInputFileAndFlags = do
+  args <- getArgs
+  let (flags, notFlags) = partition isFlag args
+  let f = case notFlags of
+        [] -> "/dev/stdin"
+        a:_ -> a
+  fmap (, map (\v -> (v !! 1, drop 2 v)) flags) (readFile f)
